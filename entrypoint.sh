e48e0333692b706668be60a0bccbd236b2e12f31
Ricardo Catalinas Jiménez <ricardo.catalinas@checkout.com>
2022-05-19 15:44:09 +0100

    Pin to Redis to v6

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ SLAVE2_PORT=$5
 SLAVE3_PORT=$6
 
 
-docker_run="docker run -d -p ${MASTER1_PORT}:6379 -p ${MASTER2_PORT}:6380 -p ${MASTER3_PORT}:6381 -p ${SLAVE1_PORT}:6382 -p ${SLAVE2_PORT}:6383 -p ${SLAVE3_PORT}:6384 --name redis-cluster vishnunair/docker-redis-cluster:latest"
+docker_run="docker run -d -p ${MASTER1_PORT}:6379 -p ${MASTER2_PORT}:6380 -p ${MASTER3_PORT}:6381 -p ${SLAVE1_PORT}:6382 -p ${SLAVE2_PORT}:6383 -p ${SLAVE3_PORT}:6384 --name redis-cluster vishnunair/docker-redis-cluster@sha256:09a85e26a7f41ea53b0bd3898899c2779276236d542c096098238e0be1f79339"
 
 sh -c "$docker_run"
